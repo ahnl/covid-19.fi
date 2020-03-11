9f1cb892052cbcfb5e0225e0ef454762c546fbb4
ahnl <you@example.com>
2020-03-11 17:58:17 +0200

    Load charts from CSV

--- www/app.js
+++ www/app.js
@@ -1,5 +1,3 @@
-
-
 var cvfGraphOptions = {
     layout: {
         padding: {
@@ -16,7 +14,6 @@ var cvfGraphOptions = {
         intersect: false,
         custom: function (model) {
             document.getElementById(this._chart.canvas.dataset.chartTooltip).innerHTML = model.title + '<br>' + model.dataPoints[0].yLabel + ' tartuntaa';
-            //console.log(model.title + ': ' + model.dataPoints[0].yLabel);
         }
     },
     hover: {
@@ -87,10 +84,11 @@ Chart.controllers.LineWithLine = Chart.controllers.line.extend({
 
 var c1 = document.getElementById('canvas1');
 var c2= document.getElementById('canvas2');
-var graphTooltip1 = document.getElementById('graphTooltip1');
-var graphTooltip2 = document.getElementById('graphTooltip2');
 var ctx = c1.getContext('2d');
 var ctx2 = c2.getContext('2d');
+var graphTooltip1 = document.getElementById('graphTooltip1');
+var graphTooltip2 = document.getElementById('graphTooltip2');
+
 
 function clearTooltips() {
     graphTooltip1.style.display = 'none';
@@ -102,44 +100,71 @@ c1.ontouchleave = clearTooltips;
 c2.onmouseleave = clearTooltips;
 c2.ontouchleave = clearTooltips;
 
-var chart = new Chart(ctx, {
-    type: 'LineWithLine',
-    data: {
-        labels: ['29.01.', '30.01.', '31.01.', '01.02.', '02.02.', '03.02.', '04.02.', '05.02.', '06.02.', '07.02.', '08.02.', '09.02.', '10.02.', '11.02.', '12.02.', '13.02.', '14.02.', '15.02.', '16.02.', '17.02.', '18.02.', '19.02.', '20.02.', '21.02.', '22.02.', '23.02.', '24.02.', '25.02.', '26.02.', '27.02.', '28.02.', '29.02.', '01.03.', '02.03.', '03.03.', '04.03.', '05.03.', '06.03.', '07.03.', '08.03.'],
-        datasets: [{
-            label: 'Tartuntoja',
-            data: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 2, 1, 1, 0, 5, 3, 4, 4, 7],
-            borderColor: '#2D5520',
+function getLastUpdated() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://api.github.com/repos/ahnl/coronavirus-finland/commits?path=total.csv&page=1&per_page=1', true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        document.getElementById('lastUpdated').innerHTML = 'Tilastot päivitetty viimeeksi ' + timeago.format(new Date(xhr.response[0].commit.author.date), 'fi');
+      }
+    };
+    xhr.send();
+}
+getLastUpdated();
 
-            fill: true,
-            backgroundColor: '#2D5520'
-        }]
-    },
-    options: cvfGraphOptions
+function dataFromCsv(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+          var rows = xhr.response.trim().split('\n');
+          var data = {labels: [], data: []};
+          for (var i = 0; i < rows.length; i++) {
+            try {
+                var row = rows[i].split(',');
+                var date = new Date(row[0]);
+                var cleanDate = date.getDate() + '.' + (date.getMonth() + 1) + '.';
+                data.labels.push(cleanDate);
+                data.data.push(row[1]);
+            }  catch (e) {
+               console.log('dataFromCsv invalid row ' + i)
+            }
+          }
+        callback(null, data);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+}
+
+function makeChart(canvasContext, label, data) {
+    new Chart(canvasContext, {
+        type: 'LineWithLine',
+        data: {
+            labels: data.labels,
+            datasets: [{
+                label: label,
+                data: data.data,
+                borderColor: '#2D5520',
+                fill: true,
+                backgroundColor: '#2D5520'
+            }]
+        },
+        options: cvfGraphOptions
+    });
+}
+dataFromCsv('https://raw.githubusercontent.com/ahnl/coronavirus-finland/master/day.csv', function(err, data) {
+    if (!err) {
+        makeChart(ctx, 'Tapaukset', data);
+    }    
 });
 
-
-var chart2 = new Chart(ctx2, {
-    type: 'LineWithLine',
-    data: {
-        labels: ['29.01.', '30.01.', '31.01.', '01.02.', '02.02.', '03.02.', '04.02.', '05.02.', '06.02.', '07.02.', '08.02.', '09.02.', '10.02.', '11.02.', '12.02.', '13.02.', '14.02.', '15.02.', '16.02.', '17.02.', '18.02.', '19.02.', '20.02.', '21.02.', '22.02.', '23.02.', '24.02.', '25.02.', '26.02.', '27.02.', '28.02.', '29.02.', '01.03.', '02.03.', '03.03.', '04.03.', '05.03.', '06.03.', '07.03.', '08.03.'],
-        datasets: [{
-            label: 'Tartuntoja yhteensä',
-            data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 5, 6, 6, 12, 15, 19, 23, 30, 30],
-            borderColor: '#2D5520',
-
-            fill: true,
-            backgroundColor: '#2D5520'
-        }]
-    },
-    options: cvfGraphOptions
+dataFromCsv('https://raw.githubusercontent.com/ahnl/coronavirus-finland/master/total.csv', function(err, data) {
+    if (!err) {
+        makeChart(ctx2, 'Tartunnat yhteensä', data);
+    }    
 });
-
-/*
-,
-    plugins: {
-        datasource: {
-            url: 'https://ahnl.github.io/coronavirus-finland/total.csv'
-        }
-    }
-    */
